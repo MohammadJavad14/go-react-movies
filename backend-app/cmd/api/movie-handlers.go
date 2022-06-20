ad8eac7e02d47666f846505317792cc6c606d826
MohammadJavad14 <mrph14@gmail.com>
2022-06-20 18:06:08 +0430

    displaying one movie

--- backend-app/cmd/api/movie-handlers.go
+++ backend-app/cmd/api/movie-handlers.go
@@ -18,8 +18,6 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("id is ", id)
-
 	movie, err := app.models.DB.Get(id)
 
 	// movie := models.Movie{
@@ -53,4 +51,20 @@ func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
 }
